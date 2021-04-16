a385db76cbb7d003a70a007e605caec130533811
Kosenko <Kosenko.2000@yandex.ru>
2021-04-16 23:01:31 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/SpringShopBase/SpringShopBaseApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringShopBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShopBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/SpringShopBase/SpringShopBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.SpringShopBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
